4617bebbd4087ddc6c99e05b89f836c018c8b1ff
zannn1019 <fauzanguci1019@gmail.com>
2026-07-21 11:49:34 +0700

    Update face_camera.py to use CUDAExecutionProvider and adjust detection size

--- face_camera.py
+++ face_camera.py
@@ -2,16 +2,17 @@ import cv2
 from insightface.app import FaceAnalysis
 
 app = FaceAnalysis(
-    providers=["CPUExecutionProvider"],
+    providers=["CUDAExecutionProvider"],
 )
 
 app.prepare(
     ctx_id=0,
-    det_size=(800, 800)
+    det_size=(320, 320)
 )
 
 camera = cv2.VideoCapture(0)
-
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 while True:
     success, frame = camera.read()    
     frame = cv2.flip(frame, 1)
@@ -23,7 +24,7 @@ while True:
     for face in faces:
         score = face.det_score
 
-        x1, x2, y1, y2 = face.bbox.astype(int)
+        x1, y1, x2, y2 = face.bbox.astype(int)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, f"Score: {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
